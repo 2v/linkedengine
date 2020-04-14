49932f05def087eaa1c508cf1e8203dca22f67e3
Thomas Buckley <tbuckley@tabwebsites.com>
2020-04-14 18:07:08 -0400

    updated project structure and adapted syntax to better fit Python standards

--- linkedin.py
+++ linkedin.py
@@ -1,28 +1,29 @@
 import csv
 from dataclasses import dataclass
-import requests, time, random
+import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
 @dataclass
 class User:
-  stage: str
-  firstname: str
-  lastname: str
-  company: str
-  position: str
-  resume: str
-  location: str
-  status: str
+    stage: str
+    first_name: str
+    last_name: str
+    company: str
+    position: str
+    resume: str
+    location: str
+    status: str
+
 
 d = webdriver.Firefox()
 d.get('https://www.linkedin.com')
 conf = open('config.txt')
-l = conf.readlines()
-USERNAME = l[0]
-PASSWORD = l[1]
-QUERY = l[2]
-FILENAME = l[3]
+lines = conf.readlines()
+USERNAME = lines[0]
+PASSWORD = lines[1]
+QUERY = lines[2]
+FILENAME = lines[3]
 
 # writer = csv.writer(open('testing.csv', 'w')) # preparing csv file to store parsing result later
 # writer.writerow(['name', 'job_title', 'schools', 'location', 'ln_url'])
@@ -40,10 +41,10 @@ password_input.send_keys(PASSWORD)
 d.find_element_by_xpath('//button[text()="Sign in"]').click()
 
 try:
-  d.find_element_by_xpath('//button[text()="Skip"]').click()
-  print("Logged in")
+    d.find_element_by_xpath('//button[text()="Skip"]').click()
+    print("Logged in")
 except:
-  print("Continuing with procedure, successfully logged in") 
+    print("Continuing with procedure, successfully logged in")
 
 d.get(QUERY)
 
@@ -53,31 +54,31 @@ SCROLL_PAUSE_TIME = 5
 last_height = d.execute_script("return document.body.scrollHeight")
 
 for i in range(3):
-  # Scroll down to bottom
-  d.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # Scroll down to bottom
+    d.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-  # Wait to load page
-  time.sleep(SCROLL_PAUSE_TIME)
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
 
-  # Calculate new scroll height and compare with last scroll height
-  new_height = d.execute_script("return document.body.scrollHeight")
-  if new_height == last_height:
-      break
-  last_height = new_height
+    # Calculate new scroll height and compare with last scroll height
+    new_height = d.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
 
 src = d.page_source
 soup = BeautifulSoup(src, 'lxml')
 
 # for testing purposes
-with open("out.html", "w") as file:
+with open("out.html", "w", encoding="utf-8") as file:
     file.write(soup.prettify())
 
 name_div = soup.find('div', {'class': 'flex-1 mr5'})
 
 name_loc = name_div.find_all('ul')
 name = name_loc[0].find('li').get_text().strip().split(' ')
-firstname = name[0]
-lastname = name[1]
+first_name = name[0]
+last_name = name[1]
 loc = name_loc[1].find('li').get_text().strip()
 profile_title = name_div.find('h2').get_text().strip()
 
@@ -89,29 +90,28 @@ a_tags = li_tags.find('a')
 
 # handling of alternate version of experience display
 if (len(a_tags.find_all('p')) < 2):
-  # special case
-  company_name = exp_section.find_all('h3')[0].find_all('span')[1].get_text().strip()
-  job_title = exp_section.find_all('h3')[1].find_all('span')[1].get_text().strip()
-
+    # special case
+    company_name = exp_section.find_all('h3')[0].find_all('span')[1].get_text().strip()
+    job_title = exp_section.find_all('h3')[1].find_all('span')[1].get_text().strip()
 else:
-  job_title = a_tags.find('h3').get_text().strip()
-  company_name = a_tags.find_all('p')[1].get_text().strip()
+    job_title = a_tags.find('h3').get_text().strip()
+    company_name = a_tags.find_all('p')[1].get_text().strip()
 
-print("First name: " + firstname)
-print("Last name: " + lastname)
+print("First name: " + first_name)
+print("Last name: " + last_name)
 print("Location: " + loc)
 print("Position: " + job_title)
 print("Company Name: " + company_name)
 
-filename = FILENAME + ".csv"
+path = FILENAME + ".csv"
 
-row = ['primary', firstname, lastname, company_name, job_title, 'resume', loc, 'status'] 
+row = ['primary', first_name, last_name, company_name, job_title, 'resume', loc, 'status']
 
 fields = ['stage', 'first_name', 'last_name', 'company', 'position', 'resume', 'location', 'status'] 
 
-with open(filename, 'w') as csvfile:  
-    csvwriter = csv.writer(csvfile)   
-    csvwriter.writerow(fields)
-    csvwriter.writerow(row) 
+with open(path, 'w') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(fields)
+    csv_writer.writerow(row)
 
-# writer.writerow([name, job_title, schools, location, ln_url])
+d.close()
